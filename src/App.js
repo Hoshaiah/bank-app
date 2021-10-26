213f0d34a91b5a26920ea8ed5509feea104b8244
Hoshaiah Domingo <domingohoshaiah@gmail.com>
2021-10-26 22:33:16 +0800

    initial banking app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let color = "blue"
+  let balance = 0
+  let user = "Hosh"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav> 
+      
+        <ul id="gennav">
+          <img src={logo} className="App-logo" alt="logo" />
+          <li>Dashboard</li>
+          <li>Wallet</li>
+          <li>Activity</li>
+          <li>Help</li>
+        </ul>
+        <ul id="lognav">
+          <li>Settings</li>
+          <li>Notifications</li>
+          <li>Log Out</li>
+        </ul>
+      </nav>
+      <header>
+          <caption>Good Evening, {user}</caption>
       </header>
+      <main>
+        <div id="balance">
+          <h1>HoshBank Balance</h1>
+          <h2>P {balance}</h2>
+        </div>
+      <div id= "actions">
+          <div id="send"> Send</div>
+          <div id="request"> Request</div>
+          <div id="link">Link Bank</div>
+          <div id="withdraw"> Withdraw</div>
+          <div id="deposit"> Deposit</div>
+      </div>
+      <div id="recentActivity">
+        <h1>Recent Activity</h1>
+        <div id ="recentActivites"></div>
+      </div>
+      </main>
+
+
+
     </div>
   );
 }
